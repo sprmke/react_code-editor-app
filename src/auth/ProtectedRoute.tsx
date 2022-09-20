8153c4e58c893a3d8aa145ceac73e68390a54cde
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-21 05:20:53 +0800

    22. [updated] creating protected routes with auto0

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,37 +1,20 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import React, { PropsWithChildren, ReactNode, useEffect } from 'react';
-import { Route, RouteProps, Routes } from 'react-router-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { ComponentType } from 'react';
 import Loading from '../components/common/Loading/Loading';
 
-const ProtectedRoute = (props: PropsWithChildren<{ [key: string]: any }>) => {
-  const { children, ...args } = props;
+interface ProtectedRouteProps {
+  component: ComponentType;
+}
 
-  // Do the next alternative code after because this is not working with new React Router v6
-  // Learn more: https://github.com/auth0/auth0-react/issues/226
-  // <Route
-  //   element={withAuthenticationRequired(children, {
-  //     onRedirecting: () => <Loading />,
-  //   })}
-  //   {...args}
-  // />;
-
-  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
-
-  useEffect(() => {
-    if (isLoading || isAuthenticated) {
-      return;
+const ProtectedRoute = ({ component }: ProtectedRouteProps) => {
+  const Component = withAuthenticationRequired(
+    component as ComponentType<any>,
+    {
+      onRedirecting: () => <Loading />,
     }
+  );
 
-    (async (): Promise<void> => {
-      await loginWithRedirect({
-        appState: {
-          returnTo: `${window.location.pathname}${window.location.search}`,
-        },
-      });
-    })();
-  }, [isLoading, isAuthenticated, loginWithRedirect]);
-
-  return isAuthenticated ? <Route {...args}>{children}</Route> : <Loading />;
+  return <Component />;
 };
 
 export default ProtectedRoute;
