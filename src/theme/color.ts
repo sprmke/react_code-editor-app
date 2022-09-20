b08f9d26e276c8fced56070ba518534dbfd336de
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-21 01:40:14 +0800

    20. configuring the app colors

---
+++ src/theme/color.ts
@@ -0,0 +1,15 @@
+export const commonColors = {
+  white: '#fff',
+  black: '#000',
+};
+
+export const appColors = {
+  background: commonColors.white,
+  font: commonColors.black,
+  primary: '#203040',
+};
+
+export const darkModeColors = {
+  background: '#232B32',
+  font: commonColors.white,
+};
