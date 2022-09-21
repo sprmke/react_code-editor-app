40fab61ab8b6588d9616d64744d440c73fe48821
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-22 04:58:36 +0800

    34. testing the files reducer pt1

---
+++ src/store/files/reducer.test.ts
@@ -0,0 +1,102 @@
+import { PayloadAction } from '@reduxjs/toolkit';
+import filesReducer, {
+  initialState,
+  setFiles,
+  addActiveFile,
+  removeActiveFile,
+  updateFileCode,
+} from './reducer';
+
+describe('files reducer', () => {
+  it('should return the initial state if no known action is provided', () => {
+    expect(filesReducer(undefined, {} as PayloadAction)).toEqual(initialState);
+  });
+
+  it('should set user files when action is setFiles', () => {
+    const userFiles = [
+      {
+        id: '1',
+        name: 'index.js',
+        relativePath: 'test/index.js',
+        code: 'console.log("hello world")',
+        extension: 'js',
+      },
+    ];
+
+    const expectedState = {
+      ...initialState,
+      activeFiles: [],
+      userFiles,
+    };
+
+    expect(filesReducer(initialState, setFiles(userFiles))).toEqual(
+      expectedState
+    );
+  });
+
+  it('should add a new file id when action is addActiveFile', () => {
+    const fileId = '1';
+    const expectedState = {
+      ...initialState,
+      activeFiles: [fileId],
+    };
+
+    expect(filesReducer(initialState, addActiveFile(fileId))).toEqual(
+      expectedState
+    );
+  });
+
+  it('should remove a file id when action is removeActiveFile', () => {
+    const fileId = '1';
+    const modifiedInitialState = {
+      ...initialState,
+      activeFiles: [fileId],
+    };
+
+    const expectedState = {
+      ...initialState,
+      activeFiles: [],
+    };
+
+    expect(
+      filesReducer(modifiedInitialState, removeActiveFile(fileId))
+    ).toEqual(expectedState);
+  });
+
+  it('should update the code of a file when action is updateFileCode', () => {
+    const payload = {
+      fileId: '1',
+      newCode: 'print("Hello World")',
+    };
+
+    const modifiedInitialState = {
+      ...initialState,
+      userFiles: [
+        {
+          id: '1',
+          code: 'console.log("Hello World")',
+          name: 'index.js',
+          relativePath: 'test/index.js',
+          extension: 'js',
+        },
+      ],
+    };
+
+    const expectedState = {
+      ...initialState,
+      userFiles: [
+        {
+          id: '1',
+          code: 'print("Hello World")',
+          name: 'index.js',
+          relativePath: 'test/index.js',
+          extension: 'js',
+        },
+      ],
+    };
+
+    expect(filesReducer(modifiedInitialState, updateFileCode(payload))).toEqual(
+      expectedState
+    );
+  });
+});
