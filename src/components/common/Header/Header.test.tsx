4bdd94662550cbd8439ce8c94bee6816c4b93d0c
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-22 03:10:19 +0800

    30. just add comments

--- src/components/common/Header/Header.test.tsx
+++ src/components/common/Header/Header.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
+
+// install new package to mock redux store
+// npm i --save-dev redux-mock-store
+// npm i --save @types/redux-mock-store
 import configureStore from 'redux-mock-store';
 import Header from './Header';
 
+// Wrap your component with Mock redux provider
 const renderHeaderComponent = async () => {
   const mockStore = configureStore();
   const store = mockStore();
@@ -22,6 +27,8 @@ describe('Header', () => {
 
     // act
     const logoTextElement = screen.queryByText(/Code Editor App/i);
+
+    // test svg existence by awaiting it and using findByTestId()
     const svgElement = await screen.findByTestId(/dark-mode-icon/i);
     const switchElement = screen.getByRole('checkbox');
     const buttonElement = screen.getByRole('button');
@@ -41,6 +48,7 @@ describe('Header', () => {
     const switchElement = screen.getByRole('checkbox', { checked: false });
 
     userEvent.click(switchElement);
+    // check switch input checked property
     expect(switchElement).toHaveProperty('checked', true);
     userEvent.click(switchElement);
     expect(switchElement).toHaveProperty('checked', false);
